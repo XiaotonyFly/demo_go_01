c3dd375002dccbbb5abc2604f6cb67e402875fdf
XiaotonyFly <614186309@qq.com>
2019-01-18 16:27:27 +0800

    修改嵌套循环

--- src/main/day-01/goLoop.go
+++ src/main/day-01/goLoop.go
@@ -42,13 +42,40 @@ func main() {
 		fmt.Printf("第 %d 个元素为 %d\n", i, value)
 	}
 
+	getSuShu()
+
 }
 
+/*
+	Go 语言嵌套循环的格式：
+
+	for [condition |  ( init; condition; increment ) | Range]{
+   		for [condition |  ( init; condition; increment ) | Range]{
+      		statement(s);
+   		}
+   		statement(s);
+	}
+*/
 func getChengFaBiao() {
 	for i := 1; i <= 9; i++ {
 		for j := 1; j <= i; j++ {
 			fmt.Printf(" %d * %d = %d ", j, i, i*j)
 		}
 		fmt.Println("")
+	}
+}
+
+func getSuShu() {
+	var i, j int
+	for i = 2; i < 100; i++ {
+		for j = 2; j <= (i / j); j++ {
+			if i%j == 0 {
+				break // 如果发现因子，则不是素数
+			}
+		}
+		if j > (i / j) {
+			fmt.Println(j)
+			fmt.Printf("%d  是素数\n", i)
+		}
 	}
 }
