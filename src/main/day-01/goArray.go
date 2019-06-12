1bb7039cc7ff2aa9ef8eca9655eb2f6da5d8b6d2
XiaotonyFly <614186309@qq.com>
2019-06-12 18:00:54 +0800

    go数组、指针以及结构体

--- src/main/day-01/goArray.go
+++ src/main/day-01/goArray.go
@@ -5,6 +5,22 @@ import (
 )
 
 func main() {
+	/*
+	   数组的声明:
+	   	var variable_name  [SIZE] variable_type
+	   多维数组声明：var variable_name  [SIZE1][SIZE2]...[SIZEN] variable_type
+
+	   数组初始化：
+	   var balance = [5]float32{1000.0, 2.0, 3.4, 7.0, 50.0}
+	   初始化数组中 {} 中的元素个数不能大于 [] 中的数字。
+
+	   如果忽略 [] 中的数字不设置数组大小，Go 语言会根据元素的个数来设置数组的大小：
+
+	    var balance = [...]float32{1000.0, 2.0, 3.4, 7.0, 50.0}
+	   或
+	    var balance = []float32{1000.0, 2.0, 3.4, 7.0, 50.0}
+
+	*/
 	var intArray [3]int
 	fmt.Println(len(intArray))
 	fmt.Println(intArray[2])
@@ -30,6 +46,10 @@ func main() {
 		fmt.Println("p 不为空指针")
 	}
 	ads(&b)
+	fmt.Println("---------------分割线-------------")
+
+	var balance = [...]float32{1, 2, 6.9, 3.4}
+	fmt.Println("balance数组长度为：", len(balance))
 }
 
 func ads(h *int) {
