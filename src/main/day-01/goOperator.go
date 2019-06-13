26e0e90d39f1c8c836e87511ddf36bbba9cb42e0
XiaotonyFly <614186309@qq.com>
2019-06-13 18:11:14 +0800

    go数组、指针以及结构体

--- src/main/day-01/goOperator.go
+++ src/main/day-01/goOperator.go
@@ -39,9 +39,16 @@ func showGxOperator() {
 		fmt.Println("a 不等于 b")
 	}
 
+	/*
+		Go 语言的取地址符是 &，放到一个变量前使用就会返回相应变量的内存地址。
+		指针声明：
+			var variable_name *variable_type
+		variable_type 为指针类型，variable_name 为指针变量名，* 号用于指定变量是作为一个指针。
+	*/
 	var p *int
 	p = &a
 	fmt.Println(p)
 	fmt.Println(&a)
+	//在指针变量前面加上 * 号（前缀）来获取指针所指向的内容。
 	fmt.Println(*p)
 }
